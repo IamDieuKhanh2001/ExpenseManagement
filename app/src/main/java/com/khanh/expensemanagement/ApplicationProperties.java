471ff4dd053eed15115652c44760270b91eaf97e
QuachDieuKhanh <quachdieukhanh@gmail.com>
2025-06-21 23:48:11 +0700

    m name update

--- app/src/main/java/com/khanh/expensemanagement/ApplicationProperties.java
+++ app/src/main/java/com/khanh/expensemanagement/ApplicationProperties.java
@@ -6,7 +6,7 @@ public class ApplicationProperties {
     public static final String DATABASE_NAME = "ExpenseManagement.db";
 
     // Database version number
-    public static final Integer DATABASE_VERSION = 7;
+    public static final Integer DATABASE_VERSION = 8;
 
     // Log file save at
     public static final String LOG_DIR = "Download/ExpenseManagement";
